0843a2762f3aa3a378f92f8ae2647625dd5e6148
NataliaLegazcue <69222918+NataliaLegazcue@users.noreply.github.com>
2022-08-24 21:19:08 -0300

    propuesta hecha en clase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_Propuesto
+{    
+    public class Program
+    {
+        private static ArrayList productos = new ArrayList();
+        private static ArrayList reviews = new ArrayList();
+        public static void Main(string[] args)
+        {
+            PopulateCatalogs();
+            Producto product = new Producto ();
+        
+            recipe.AddProducto (“Pera”, 50, new Producto(GetReviews (5,"Recomendable"));
+            recipe.AddProducto(“Manzana”,32,new Producto (GetReviews(4,"Relación calidad precio correcta"));
+            Console.WriteLine(recipe.ConvertToJson());
+        }
+        private static void ListadoProductos()
+        
+        {
+            AddProductoToListado ("Peras", 50);
+            AddProductToCatalog("Manzanas", 32);
+
+        }
+    }
+}            
+        
